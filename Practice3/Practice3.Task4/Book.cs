85334b0fc3cf36671432dbc753b39fab5a892734
srgsv <srg.svt@gmail.com>
2025-05-12 21:54:15 +0400

    Create Practice3.Task4

---
+++ Practice3/Practice3.Task4/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.Task4
+{
+    internal class Book
+    {
+        private string _avtor;
+        private string _nameBook;
+        public Book() {
+            _avtor = "Неизвестный автор";
+            _nameBook = "Неизвечтное название";
+        }
+        public Book(string avtor, string nameBook)
+        { 
+            _avtor = avtor;
+            _nameBook = nameBook;
+        }
+
+        public void Print() => Console.WriteLine($"автор книги - {_avtor}, название книги - {_nameBook}");
+    }
+}
