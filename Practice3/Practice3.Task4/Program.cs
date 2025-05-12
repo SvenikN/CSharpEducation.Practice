5f3d908425a362cb75b65032a8cd090235ce8af7
srgsv <srg.svt@gmail.com>
2025-05-12 23:17:32 +0400

    Create Practice3.Task5

--- Practice3/Practice3.Task4/Program.cs
+++ Practice3/Practice3.Task4/Program.cs
@@ -2,7 +2,7 @@
 using Practice3.Task4;
 
 Book book = new Book();
-Book book1 = new Book("Роулинг", "Гарри Поттер");
+Book book1 = new Book("Гарри Поттер", "Роулинг");
 
 book.Print();
 book1.Print();
