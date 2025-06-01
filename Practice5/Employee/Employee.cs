0d217ca1ea49af81a7bab3a500fce55fcb543731
srgsv <srg.svt@gmail.com>
2025-06-01 23:43:53 +0400

    Create Practice 5

---
+++ Practice5/Employee/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        
+        public Employee() 
+        {
+            Name = "Не известно";
+            Salary = 0;
+        }
+        public Employee(string name, double salary) 
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public virtual double CalculateBonus() {
+            return Salary*0.1;
+        }
+    }
+}
---
+++ Practice5/Logger/ConsoleLogger.cs
@@ -0,0 +1,29 @@
+﻿using Logger;
+using System;
+
+public class ConsoleLogger : ILogger
+{
+    public void Trace(string message) => Log(message, LogLevel.Trace);
+    public void Info(string message) => Log(message, LogLevel.Info);
+    public void Debug(string message) => Log(message, LogLevel.Debug);
+    public void Warning(string message) => Log(message, LogLevel.Warning);
+    public void Error(string message) => Log(message, LogLevel.Error);
+    public void Fatal(string message) => Log(message, LogLevel.Fatal);
+
+    public void Log(string message, LogLevel level)
+    {
+        // Получаем имя класса, который вызвал логгер
+        string className = GetCallingClassName();
+
+        string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {className} | {level} | {message}";
+        Console.WriteLine(logMessage);
+    }
+
+    private string GetCallingClassName()
+    {
+        var stackTrace = new System.Diagnostics.StackTrace();
+        var frame = stackTrace.GetFrame(2); // 0 - GetCallingClassName, 1 - Log, 2 - вызывающий метод
+        var method = frame?.GetMethod();
+        return method?.DeclaringType?.Name ?? "UnknownClass";
+    }
+}
